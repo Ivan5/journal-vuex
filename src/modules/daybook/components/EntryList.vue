7d9df45acd18919e76331a80a08cab08569c3c43
Ivan Jaramillo <ivan@aiwifi.io>
2022-05-10 22:01:04 -0500

    Add structure and some files for layout and component about daybook section

---
+++ src/modules/daybook/components/EntryList.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>EntryList</h1>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hola mndo</h1>
+    <button @click="gotoDaybook" class="btn btn-primary">Daybook</button>
   </div>
 </template>
 
@@ -8,6 +10,12 @@
 export default {
   name: 'Home',
   components: {
-  }
+    
+  },
+  methods: {
+      gotoDaybook(){
+        this.$router.push({name: 'daybook'})
+      }
+    }
 }
 </script>
