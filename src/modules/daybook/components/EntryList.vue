2a954be2f15fbf411df3c55a6c55e05163aa9f48
Ivan Jaramillo <ivan@aiwifi.io>
2022-06-05 18:39:14 -0500

    Add some views to the project

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>EntryList</h1>
+    <div class="entry-list-cointainer">
+        <div class="px-2 pt-2">
+            <input type="text" class="form-control" placeholder="Buscar entrada">
+        </div>
+        <div class="entry-scrollarea">
+            <Entry v-for="entry in 20" :key="entry"></Entry>
+        </div>
+    </div>
 </template>
+<script>
+import {defineAsyncComponent} from 'vue';
+
+export default {
+    components:{
+        Entry: defineAsyncComponent(() => import('./Entry.vue'))
+    }
+}
+</script>
+<style lang="scss" scoped>
+input{
+    height: 25px;
+}
+.entry-list-container{
+    border-right: 1px solid crimson;
+    height: calc(100vh - 56px);
+}
+
+.entry-scrollarea{
+    height: calc(100vh - 110px);
+    overflow: scroll;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@ export default {
   },
   methods: {
       gotoDaybook(){
-        this.$router.push({name: 'daybook'})
+        this.$router.push({name: 'no-entry'})
       }
     }
 }
